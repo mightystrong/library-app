6aab1a207ce5c5f412db0a3541d548ebeb46e464
Michael Price <mike@mightystrongmedia.com>
2017-12-11 10:37:49 -0700

    Finish contact form.

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -7,5 +7,14 @@ export default Controller.extend({
   emailValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
   messageValid: Ember.computed.gte('message.length', 5),
   isValid: Ember.computed.and('emailValid', 'messageValid'),
-  isDisabled: Ember.computed.not('isValid')
+  isDisabled: Ember.computed.not('isValid'),
+
+  actions: {
+    sendMessage() {
+      alert(`Sending your message: ${this.get('emailAddress')} - ${this.get('message')}`);
+      this.set('responseMessage', `Thank you! We got your message and we'll get in touch soon`);
+      this.set('emailAddress', '');
+      this.set('message', '');
+    }
+  }
 });
